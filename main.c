2ba35b53a20221b9ed6d944501fa2492e7c24cd1
Baptiste DEVESSIER <bdevessi@student.42.fr>
2018-11-23 12:02:15 +0100

    Use puts directly

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bdevessi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 16:48:57 by bdevessi          #+#    #+#             */
-/*   Updated: 2018/11/23 11:34:08 by bdevessi         ###   ########.fr       */
+/*   Updated: 2018/11/23 12:01:59 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int		main()
 	char	*line = NULL;
 	int	fd = open("./text.txt", O_RDONLY);
 	while (get_next_line(fd, &line) > 0)
-		printf("%s", line);
+		puts(line);
 }
